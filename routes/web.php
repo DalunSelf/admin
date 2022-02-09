af9314f16a9e3a0d351671ea6da6e58eb1b20dce
DalunSelf <53163317+DalunSelf@users.noreply.github.com>
2022-02-09 20:08:35 +0800

    路由移除

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,41 @@
 <?php
 
-use Filament\Facades\Filament;
-use Filament\Http\Controllers\AssetController;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Route;
+// use Filament\Facades\Filament;
+// use Filament\Http\Controllers\AssetController;
+// use Illuminate\Http\RedirectResponse;
+// use Illuminate\Support\Facades\Route;
 
-Route::domain(config('filament.domain'))
-    ->middleware(config('filament.middleware.base'))
-    ->name('filament.')
-    ->prefix(config('filament.path'))
-    ->group(function () {
-        if ($loginPage = config('filament.auth.pages.login')) {
-            Route::get('/login', $loginPage)->name('auth.login');
-        }
+// Route::domain(config('filament.domain'))
+//     ->middleware(config('filament.middleware.base'))
+//     ->name('filament.')
+//     ->prefix(config('filament.path'))
+//     ->group(function () {
+//         if ($loginPage = config('filament.auth.pages.login')) {
+//             Route::get('/login', $loginPage)->name('auth.login');
+//         }
 
-        Route::get('/core/assets/{file}', AssetController::class)->where('file', '.*')->name('asset');
+//         Route::get('/core/assets/{file}', AssetController::class)->where('file', '.*')->name('asset');
 
-        Route::middleware(config('filament.middleware.auth'))->group(function (): void {
-            Route::name('pages.')->group(function (): void {
-                foreach (Filament::getPages() as $page) {
-                    Route::group([], $page::getRoutes());
-                }
-            });
+//         Route::middleware(config('filament.middleware.auth'))->group(function (): void {
+//             Route::name('pages.')->group(function (): void {
+//                 foreach (Filament::getPages() as $page) {
+//                     Route::group([], $page::getRoutes());
+//                 }
+//             });
 
-            Route::name('resources.')->group(function (): void {
-                foreach (Filament::getResources() as $resource) {
-                    Route::group([], $resource::getRoutes());
-                }
-            });
+//             Route::name('resources.')->group(function (): void {
+//                 foreach (Filament::getResources() as $resource) {
+//                     Route::group([], $resource::getRoutes());
+//                 }
+//             });
 
-            Route::get('/logout', function (): RedirectResponse {
-                Filament::auth()->logout();
+//             Route::get('/logout', function (): RedirectResponse {
+//                 Filament::auth()->logout();
 
-                session()->invalidate();
-                session()->regenerateToken();
+//                 session()->invalidate();
+//                 session()->regenerateToken();
 
-                return redirect()->route('filament.auth.login');
-            })->name('auth.logout');
-        });
-    });
+//                 return redirect()->route('filament.auth.login');
+//             })->name('auth.logout');
+//         });
+//     });
